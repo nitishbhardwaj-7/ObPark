71f2d40d3c76f5c5a22fa8ab0e1abf936804b754
Nitish Bhardwaj <nitish@obrive.com>
2026-03-01 17:59:52 +0530

    fixed

--- src/components/Sections/RiveCard.tsx
+++ src/components/Sections/RiveCard.tsx
@@ -1,12 +1,6 @@
-import Image from 'next/image';
 import React from 'react'
 
-type RiveCardProps = {
-  image: string;
-  alt?: string
-}
-
-function RiveCard({image , alt = 'CardImage'} : RiveCardProps) {
+function RiveCard() {
   return (
     <div className='h-42 md:h-77 w-44 md:w-77 bg-[#074038] rounded-2xl'>
 
