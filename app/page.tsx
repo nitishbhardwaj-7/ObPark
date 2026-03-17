08697d8f7e88051a09972f00a548b8fc57aa9024
Nitish Bhardwaj <nitish@obrive.com>
2026-03-17 14:00:24 +0530

    CarAnimationAdd

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,11 @@ function page() {
       <div className='md:flex items-center justify-center gap-10 pt-15'>
     <GreenCard title='Smart Parking Solution' description='Our Smart Parking Solution offers fast vehicle access within 3 seconds, automatic transaction recording for hassle-free payments, and real-time parking spot availability to minimize search time.visits stress-free and convenient.' imageSrc='/images/card1img.jpg' imgAlt=''/>
     <GreenCard title='Access Management System' description='Our Smart Parking Solution offers fast vehicle access within 3 seconds, automatic transaction recording for hassle-free payments, and real-time parking spot availability to minimize search time.visits stress-free and convenient.' imageSrc='/images/card2img.jpg' imgAlt=''/>
+
+
       </div>
+
+      <CarAnimation/>
 
     </div>
 
